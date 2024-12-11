5a4f5b9ab65ef02084b1684076fa047461835739
maldikurniawan <akurniawan2102@gmail.com>
2024-12-11 16:33:39 +0700

    add favicon

--- src/components/atoms/Menu.jsx
+++ src/components/atoms/Menu.jsx
@@ -1,7 +1,7 @@
 const Menu = ({ onClick }) => {
   return (
     <div
-      className="relative flex items-center overflow-hidden justify-center h-screen bg-cover bg-center max-w-sm mx-auto"
+      className="relative flex items-center justify-center h-screen bg-cover bg-center max-w-sm mx-auto"
       style={{ backgroundImage: "url('assets/images/tetris.jpeg')" }}
     >
       {/* Background Overlay */}
@@ -9,7 +9,9 @@ const Menu = ({ onClick }) => {
 
       {/* Main Menu */}
       <div className="relative z-10 text-center rounded-lg shadow-lg">
-        <h1 className="text-5xl mb-[420px] font-extrabold bg-gradient-to-r from-red-600 via-green-600 to-blue-600 inline-block text-transparent bg-clip-text tracking-widest">TETRIS</h1>
+        <h1 className="text-5xl mb-[420px] font-extrabold bg-gradient-to-r from-red-600 via-green-600 to-blue-600 inline-block text-transparent bg-clip-text tracking-widest">
+          TETRIS
+        </h1>
 
         <button
           onClick={onClick}
@@ -18,21 +20,10 @@ const Menu = ({ onClick }) => {
           Play
         </button>
 
-        {/* Music Toggle */}
-        <div className="flex items-center justify-center space-x-2">
-          <input
-            type="checkbox"
-            id="toggle-music"
-            className="w-4 h-4 text-blue-500 border-gray-300 rounded focus:ring focus:ring-blue-300"
-          />
-          <label htmlFor="toggle-music" className="text-white font-bold">
-            Turn On Music
-          </label>
-        </div>
-
-        {/* Follow Section */}
-        <div className="space-y-2 mt-6">
-          <p className="text-white tracking-widest cursor-pointer hover:text-purple-300 font-bold">Follow Me Here!</p>
+        <div className="justify-center">
+          <a href="https://maldikurniawan.netlify.app/" target="_blank" className="text-white tracking-widest cursor-pointer hover:text-purple-300 font-bold">
+            Follow Me Here!
+          </a>
         </div>
       </div>
     </div>
